6ca1cfa0efe3ccf8a279743a097d977ea9b918cc
Mathieu Grenier <grenmath@hotmail.com>
2024-03-27 10:05:55 -0400

    fix: revised cancel call and effect

--- src/app/toto/components/dashboard/dashboard-totos.component.ts
+++ src/app/toto/components/dashboard/dashboard-totos.component.ts
@@ -1,6 +1,9 @@
-import {Component} from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
 import { TotoComponent } from '../toto/toto.component';
 import { CommonModule } from '@angular/common';
+import { Store } from '@ngrx/store';
+import { TotoStateInterface } from '../../types/totoState.interface';
+import { totoActions } from '../../store/actions';
 
 @Component({
   selector: 'mc-dashboard-totos',
@@ -9,7 +12,8 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule, TotoComponent],
 })
-export class DashboardTotosComponent {
+export class DashboardTotosComponent implements OnDestroy{
+
   componentsList: {ids: string[]; name: string}[] = [
     {ids: ['1', '2'], name: 'component1'},
     {ids: ['4', '2'], name: 'component2'},
@@ -24,4 +28,12 @@ export class DashboardTotosComponent {
     ];
     console.log('this.componentsList: ', this.componentsList);
   }
+
+  constructor(private store: Store<{toto: TotoStateInterface}>) {}
+
+  ngOnDestroy(): void {
+    console.log('DashboardTotosComponent destroyed');
+    this.store.dispatch(totoActions.cancel());
+  }  
+  
 }
